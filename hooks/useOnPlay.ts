9900c0fd9e07a38ba5718403fadfeb174dcd7ed8
Sably <massdownup@gmaill.com>
2023-06-09 15:42:39 +0330

    first commit

---
+++ hooks/useOnPlay.ts
@@ -0,0 +1,31 @@
+import { Song } from "@/types";
+
+import usePlayer from "./usePlayer";
+import useSubscribeModal from "./useSubscribeModal";
+import useAuthModal from "./useAuthModal";
+import { useUser } from "./useUser";
+
+const useOnPlay = (songs: Song[]) => {
+  const player = usePlayer();
+  const subscribeModal = useSubscribeModal();
+  const authModal = useAuthModal();
+  const { subscription, user } = useUser();
+
+  const onPlay = (id: string) => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    //Todo: uncomment this
+    if (!subscription) {
+      return subscribeModal.onOpen();
+    }
+
+    player.setId(id);
+    player.setIds(songs.map((song) => song.id));
+  }
+
+  return onPlay;
+};
+
+export default useOnPlay;
